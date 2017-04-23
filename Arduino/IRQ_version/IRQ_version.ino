767b6a218236c8f488ab93521dffc1ccba77a459
IdeoG <ideogaib@gmail.com>
2017-04-23 14:08:52 +0300

    fixed bugs with start

--- Arduino/IRQ_version/IRQ_version.ino
+++ Arduino/IRQ_version/IRQ_version.ino
@@ -4,13 +4,13 @@
 #include <CurieBLE.h>
 #include <CurieIMU.h>
 #include <MadgwickAHRS.h>
-//#include <avr/wdt.h>    // watchdog timer; reset system if no answer
 
 #define SERVICE_UUID  ("19B10000-E8F2-537E-4F6C-D104768A1214")
 #define ANGLE_UUID    ("19B10001-E8F2-537E-4F6C-D104768A1214")
-#define TIME_US       10000   // frequency equal to 100 Hz
+#define SWITCH_UUID    ("19B10002-E8F2-537E-4F6C-D104768A1214")
+#define TIME_US       5000   // frequency equal to 200 Hz
 
-bool connected = false; 
+bool connectedBLE = false; 
 unsigned char data = 0;
 unsigned char count = 0;
 int aix, aiy, aiz;
@@ -19,50 +19,18 @@ float ax, ay, az;
 float gx, gy, gz;
 float pitch;
 
-Madgwick filter;
+
 BLEPeripheral blePeripheral; 
 BLEService eService(SERVICE_UUID); 
 
 BLEUnsignedCharCharacteristic angle_char(ANGLE_UUID, BLERead);
+BLEUnsignedCharCharacteristic switch_char(SWITCH_UUID, BLEWrite);
 
+Madgwick filter;
 
-void bleDeviceDisconnectHandler(BLEDevice central);
-void bleDeviceConnectHandler(BLEDevice central);
-static unsigned char exchange_data(void);
-static float convertRawGyro(int gRaw);
-static float convertRawAcceleration(int aRaw);
-
-void setup() {
-//  wdt_enable (WDTO_1S);  // reset after one second, if no "pat the dog" received
-  
-  pinMode(13, OUTPUT);
-
-  CurieIMU.begin();
-  CurieIMU.setGyroRate(100);     
-  CurieIMU.setAccelerometerRate(100);
-  CurieIMU.setAccelerometerRange(2);
-  CurieIMU.setGyroRange(250);
-  
-  filter.begin(100);
-  
-  BLE.begin();
-  BLE.setLocalName("eTrain");
-  BLE.setAdvertisedService(eService);
-  
-  eService.addCharacteristic(angle_char);
-  angle_char.setValue(0);
-  
-  BLE.setEventHandler(BLEConnected, BLE_Connected_IRQ);
-  BLE.setEventHandler(BLEDisconnected, BLE_Disconnected_IRQ);
-  
-  BLE.addService(eService);
-}
-
-void loop() { 
-  //wdt_reset();  // gimme 1sec for all
-}
 
 static unsigned char exchange_data(void) {
+  
   CurieIMU.readMotionSensor(aix, aiy, aiz, gix, giy, giz);
       
   ax = convertRawAcceleration(aix);
@@ -74,6 +42,7 @@ static unsigned char exchange_data(void) {
     
   filter.updateIMU(gx, gy, gz, ax, ay, az);
   pitch = filter.getPitch();
+  
   int pitch_int = static_cast<int>(pitch);
   unsigned char pitch_char = static_cast<unsigned char>(abs(pitch_int));
   return pitch_char;
@@ -91,28 +60,68 @@ static float convertRawGyro(int gRaw) {
   return g;
 }
 
-void BLE_Connected_IRQ(BLEDevice central) {
+void BLE_connectedBLE_IRQ(BLEDevice central) {
   digitalWrite(13, HIGH);
   CurieTimerOne.start(TIME_US, &IMUHandler_IRQ);
-  connected = true;
+  connectedBLE = true;
 }
 
-void BLE_Disconnected_IRQ(BLEDevice central) {
+void BLE_DisconnectedBLE_IRQ(BLEDevice central) {
   digitalWrite(13, LOW);
   CurieTimerOne.kill();
-  connected = false;
+  connectedBLE = false;
 }
 
 void IMUHandler_IRQ() {
-  // callback function
-  data = exchange_data();
-  count++;
-  if ( count > 10 ) {
-    count = 0;
-    angle_char.setValue(data); 
+  if ( switch_char.value() ) {
+    data = exchange_data();
+    count++;
+    if ( count > 10 ) {
+      count = 0;
+      angle_char.setValue(data); 
+      BLE.poll();
+    }
+    CurieTimerOne.restart(TIME_US);
   }
-  CurieTimerOne.restart(TIME_US);
 }
+
+
+
+void setup() {
+
+  pinMode(13, OUTPUT);
+
+  CurieIMU.begin();
+  CurieIMU.setGyroRate(200);     
+  CurieIMU.setAccelerometerRate(200);
+  CurieIMU.setAccelerometerRange(2);
+  CurieIMU.setGyroRange(250);
+  
+  filter.begin(200);
+
+  
+  BLE.begin();
+  
+  BLE.setLocalName("eTrain");
+  BLE.setAdvertisedService(eService);
+  
+  eService.addCharacteristic(angle_char);
+  eService.addCharacteristic(switch_char);
+  angle_char.setValue(0);
+  switch_char.setValue(0);
+
+  BLE.addService(eService);
+  BLE.advertise();
+  
+  BLE.setEventHandler(BLEConnected, BLE_connectedBLE_IRQ);
+  BLE.setEventHandler(BLEDisconnected, BLE_DisconnectedBLE_IRQ);
+
+}
+
+void loop() { 
+ 
+}
+
 
 /* Текущие проблемы:
    1.Для правильной загрузки после включения платы
